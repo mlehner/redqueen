d51fbaeee79aab127f4487756505f87dd7ee0e64
Matt Lehner <mlehner@gmail.com>
2013-11-23 14:15:53 -0500

    python script and sqlite3 tweaks
    created a python daemon for talking with the XBee
    use sqlite by default

--- app/config/config.php
+++ app/config/config.php
@@ -2,18 +2,18 @@
 
 return new \Phalcon\Config(array(
     'database' => array(
-        'adapter'     => 'Sqlite',
-        'dbname'      => '/home/matt/buffalolab/redqueen/redqueen.sqlite',
+        'adapter'           => 'Sqlite',
+        'dbname'            => realpath(__DIR__ . '/../../data/redqueen.sqlite'),
     ),
     'application' => array(
-        'controllersDir' => __DIR__ . '/../../app/Controllers/',
-        'modelsDir'      => __DIR__ . '/../../app/Models/',
-        'formsDir'       => __DIR__ . '/../../app/Forms/',
-        'viewsDir'       => __DIR__ . '/../../app/Views/',
-        'validatorDir'       => __DIR__ . '/../../app/Validators/',
-        'pluginsDir'     => __DIR__ . '/../../app/plugins/',
-        'libraryDir'     => __DIR__ . '/../../app/library/',
-        'cacheDir'       => __DIR__ . '/../../app/cache/',
-        'baseUri'        => '/',
+        'controllersDir'    => __DIR__ . '/../../app/Controllers/',
+        'modelsDir'         => __DIR__ . '/../../app/Models/',
+        'formsDir'          => __DIR__ . '/../../app/Forms/',
+        'viewsDir'          => __DIR__ . '/../../app/Views/',
+        'validatorDir'      => __DIR__ . '/../../app/Validators/',
+        'pluginsDir'        => __DIR__ . '/../../app/plugins/',
+        'libraryDir'        => __DIR__ . '/../../app/library/',
+        'cacheDir'          => __DIR__ . '/../../app/cache/',
+        'baseUri'           => '/',
     )
 ));
--- bin/redqueen.py
+++ bin/redqueen.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python2
 
 import sqlite3
+from xbee import ZigBee
+import serial
+from struct import pack
+import argparse
 
-conn = sqlite3.connect('/home/matt/buffalolab/redqueen/redqueen.sqlite')
+parser = argparse.ArgumentParser(description='RedQueen door system daemon.')
+parser.add_argument('--baud-rate', type=int, default=115200)
+parser.add_argument('--serial-port', default='ttyUSB0')
+parser.add_argument('--database', required=True)
 
-c = conn.cursor()
+args = parser.parse_args()
 
-code = 'blah'
-pin = 1234
+conn = sqlite3.connect(args.database)
+ser = serial.Serial( '/dev/%s' % args.serial_port, args.baud_rate)
+xbee = ZigBee(ser)
 
-c.execute('SELECT * FROM cards WHERE code = ? AND pin = ?', (code, pin))
+# Continuously read and print packets
+while True:
+    try:
+        response = xbee.wait_read_frame()
+        print response
 
-card = c.fetchone()
+        if 'rf_data' in response:
+            card, pin = response['rf_data'].split(':')
 
-if card == None:
-    print "No card found"
-else:
-    print card
+            print "Card ", card, " PIN ", pin
+
+            c = conn.cursor()
+            c.execute('SELECT * FROM cards WHERE code = ? AND pin = ?', (code, pin))
+            card = c.fetchone()
+
+            if card == None:
+                print "No card found"
+            else:
+                print card
+                print { 'data': pack('>bL', 0, 5) }
+                xbee.send('tx', dest_addr=response['source_addr'], dest_addr_long=response['source_addr_long'], data=pack('>bL', 0, 5))
+
+    except KeyboardInterrupt:
+        break
+
+conn.close()
+ser.close()
